0a7c7930923ed8eaf1307130d2d7670f1bc9f075
TristanKatte <tristankatte@hotmail.com>
2024-09-17 15:54:57 +0200

    styling en basis toegevoegd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import '../styles/style.css'
+</script>
+
+
+<main>
+
+    <article>
+  
+
+        <section>
+          <figure>
+            <img src="images/profielfoto-zw.jpg" class="image1" alt="foto van mijzelf">
+            <img src="images/logo_groen-300x265.png" class="image2" alt="personal logo">
+            <figcaption class="typewriter">9 november 1998</figcaption>
+          </figure>
+        </section>
+      
+        <section>
+          <h1 class="glow">Over mij</h1>
+          <!-- <p><%= data.bio %></p> -->
+      
+          <h2 class="glow">Mijn vaardigheden</h2>
+          <ul>
+            <li>Adobe Creative Cloud</li>
+            <li>Figma</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Wordpress</li>
+          </ul>
+      
+          <nav class="effect" id="effect">
+            <a href="mailto:tristankatte@hotmail.com">e-mail</a>
+            <a href="https://tristankatte.nl/nieuw_portfolio/" target="_blank"
+              >Mijn website</a
+            >
+          </nav>
+        </section>
+      
+      </article>
+
+</main>
