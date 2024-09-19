82cbe1941b8253abde1fe183316764002a9390a7
TristanKatte <tristankatte@hotmail.com>
2024-09-19 15:02:10 +0200

    gsap installed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,25 @@
 <script>
   /** @type {import('./$types').PageData} */
     import'../styles/style.css'
+    import'../scripts/script'
     import foto from '$lib/assets/profielfoto-zw.jpg';
+    import { gsap } from "gsap";
+    
+import { CustomEase } from "gsap/CustomEase";
+import { RoughEase, ExpoScaleEase, SlowMo } from "gsap/EasePack";
+    
+import { Flip } from "gsap/Flip";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Observer } from "gsap/Observer";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { Draggable } from "gsap/Draggable";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { EaselPlugin } from "gsap/EaselPlugin";
+import { PixiPlugin } from "gsap/PixiPlugin";
+import { TextPlugin } from "gsap/TextPlugin";
+
+
+gsap.registerPlugin(Flip,ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin,EaselPlugin,PixiPlugin,TextPlugin,RoughEase,ExpoScaleEase,SlowMo,CustomEase);
     export let data;
     let people = data.people
     export let peep = people? people.find(person => person.id == 54) : 'not found';
@@ -9,11 +27,10 @@
 </script>
 
 
-
-
-    <article>
-  
-
+<div id="top">
+    <div class="perspective">
+    <article class="card">
+      <div class="thumb">
         <section>
           <figure>
             <img alt="foto van mijzelf" src="{foto}">
@@ -44,6 +61,9 @@
             
           </nav>
         </section>
+      </div>
       
       </article>
+      </div>
+    </div>
 
